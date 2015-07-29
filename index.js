270af32bf8b9b4221cf18e786186607dc698c17a
Michał Budzyński <michal@virtualdesign.pl>
2015-07-29 01:01:00 +0100

    :ram: show list of tables as accordeon

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
 var drjoint = (function(){
 'use strict';
-
   $.get('sql/opencart.json', function (data) {
+    var sourceDB = $('<div></div>');
+    sourceDB.addClass('db-container');
+    var fieldElement;
+    sourceDB.appendTo(document.body);
+
     // render table names
     for (var table in data) {
-      $('body').append(createTable(table, data[table]));
+      sourceDB.append('<h3>' + table + '</h3>');
+      fieldElement = $('<div></div>');
+      fieldElement.appendTo(sourceDB);
+      $.each(data[table], function(index, field){
+        fieldElement.append($('<li></li>').text(field));
+      });
     }
+
+    sourceDB.accordion({
+      active: false,
+      collapsible: true,
+      heightStyle: 'content'
+    });
+
   });
 
   // nanoajax.ajax('sql/prestashop.json', function (code, responseText) {
@@ -13,17 +29,3 @@ var drjoint = (function(){
   //   document.body.innerHTML += '<br/>prestashop tables: ' + Object.keys(data).length;
   // });
 })();
-
-var createTable = function(name, fields){
-  var el = $('<div></div>');
-  var fieldElement;
-  el.text(name);
-
-  $.each(fields, function(index, field){
-    fieldElement = $('<div></div>');
-    fieldElement.css({'backgroundColor' : '#F0F'});
-    fieldElement.text(field);
-    fieldElement.appendTo(el);
-  });
-  return el;
-};
